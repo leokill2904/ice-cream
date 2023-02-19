b397a589dcbe8e31e3673ece458aa4d1b0ffd60b
Leonid Tanasiienko <tanasyenko029@gmail.com>
2023-02-19 17:24:06 +0200

    add section gallery

---
+++ src/js/gallery-items.js
@@ -0,0 +1,44 @@
+export const galleryItems = [
+    {
+      preview:
+        './images/gallery/original-adaptive/gallery-1.jpg',
+      original:
+        './images/gallery/gallery-1.jpg',
+      description: 'Hokkaido Flower',
+    },
+    {
+      preview:
+        './images/gallery/original-adaptive/gallery-2.jpg',
+      original:
+        './images/gallery/gallery-2.jpg',
+      description: 'Container Haulage Freight',
+    },
+    {
+      preview:
+        './images/gallery/original-adaptive/gallery-3.jpg',
+      original:
+        './images/gallery/gallery-3.jpg',
+      description: 'Aerial Beach View',
+    },
+    {
+      preview:
+        './images/gallery/original-adaptive/gallery-4.jpg',
+      original:
+        './images/gallery/gallery-4.jpg',
+      description: 'Flower Blooms',
+    },
+    {
+      preview:
+        './images/gallery/original-adaptive/gallery-5.jpg',
+      original:
+        './images/gallery/gallery-5.jpg',
+      description: 'Alpine Mountains',
+    },
+    {
+      preview:
+        './images/gallery/original-adaptive/gallery-6.jpg',
+      original:
+        './images/gallery/gallery-6.jpg',
+      description: 'Mountain Lake Sailing',
+    },
+];
